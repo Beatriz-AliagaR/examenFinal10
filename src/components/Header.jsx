30c3faf06b12325c5475a3f4a6c8c9044ab16e6e
Beatriz Aliaga <baliagar@est.emi.edu.bo>
2022-06-17 11:39:30 -0400

    commit final

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Home } from './Home'
+import { About } from './About'
+import { Dashboard } from './Dashboard'
+
+function Header() {
+  return (
+    <div className='header-container'>
+        
+    </div>
+  )
+}
+
+export {Header}
